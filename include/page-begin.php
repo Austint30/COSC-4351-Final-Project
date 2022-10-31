93882950a1f59d81cf9e24ad3aba40c6de07ec96
Austin Thibodeaux <austinthibodeaux@gmail.com>
2022-10-31 00:05:10 -0500

    Improved organization

--- include/page-begin.php
+++ include/page-begin.php
@@ -15,7 +15,7 @@
 
     <!-- Bootstrap stylesheet -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="restaurant.css" >
+    <link rel="stylesheet" href="css/restaurant.css" >
 
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>
