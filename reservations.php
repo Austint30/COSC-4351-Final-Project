75e6051ddde7973830829ba28417ea59fafda340
Zaid Kidwai <zkidwai@cougarnet.uh.edu>
2022-11-17 19:32:28 -0600

    Update reservations.php

--- reservations.php
+++ reservations.php
@@ -295,12 +295,12 @@
         let count = 0;
 
         //Loop through ccNumber. Starts at the beginning of the number and begins doubling from the first number.
-        if(cclength % 2 == 0)
+        if(cclength % 2 === 0)
         {
             for(let i = 0; i < cclength; i++)
             {
-                let currDigit = parseInt(ccNumber[i]);
-                if (i % 2 == 0) // I only want to double every other number, starts doubling with the second-to-last number. I don't want to double the last number.
+                let currDigit = parseInt(ccNumber[i]); //have to do this because ccNumber[i] is actually a char 
+                if (i % 2 == 0) //Only multiply every other num by 2, starts doubling with the second-to-last number. Do not do the last number
                 {
                     if ((currtDigit *= 2) > 9)
                     {
@@ -321,15 +321,13 @@
             for(let i = cclength - 1 ; i >= 0; i--)
             {
                 let currDigit = parseInt(ccNumber[i]);
-                if ((i - 1) % 2 == 0) // I only want to double every other number, starts doubling with the second-to-last number. I don't want to double the last number.
+                if ((i - 1) % 2 == 0) 
                 {
                     if ((currDigit *= 2) > 9)
                     {
-                        // Separate the number into component parts and then add them together.
                         let trailingNumber = currDigit % 10;
                         let firstNumber = parseInt(currDigit / 10);
 
-                        // If currentDigit was 18 then currentDigit is now 9.
                         currDigit = firstNumber + trailingNumber;
                     }
                 }
